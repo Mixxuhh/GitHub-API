3934a8ea3ba13dc801c2880eedaa40028c28ed10
Mixxuhh <wwcrase@gmail.com>
2025-04-24 17:18:20 -0600

    some styling changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,16 +4,10 @@ const Nav = () => {
   return (
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold">
-          Candidate Search
-        </Link>
+        <Link to="/" className="text-xl font-bold"></Link>
         <div className="space-x-4">
-          <Link to="/" className="hover:text-gray-300">
-            Search
-          </Link>
-          <Link to="/saved" className="hover:text-gray-300">
-            Saved Candidates
-          </Link>
+          <Link to="/" className="hover:text-gray-300"></Link>
+          <Link to="/saved" className="hover:text-gray-300"></Link>
         </div>
       </div>
     </nav>
